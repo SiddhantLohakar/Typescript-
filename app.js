7ed692d1713eef62814e6fba433bc5649ebe8d1a
Siddhant Lohakar <siddhantlohakar12@gmail.com>
2025-09-05 11:50:51 +0530

    Learnt Variable assignment

---
+++ app.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a = 20;
+let b = 30;
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,16 @@
+// Number
+let a:number = 20;
+let b:number = 30;
+
+// String
+let str:string = "Siddhant"
+
+// Boolean
+let isLoggedIn: boolean = true
+
+// null
+let keepNull:null = null
+
+// undefined
+let notDefined:undefined;
+
